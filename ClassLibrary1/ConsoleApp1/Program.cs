6cfaad33e9e73f541f73e145be3f50cf9af3bb1c
Harsh Jain <harjain@microsoft.com>
2016-11-22 08:57:28 +0530

    Repro Bug for resources.

---
+++ ClassLibrary1/ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using ClassLibrary1.Resources;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+        Console.WriteLine(Resources.String1);
+
+    }
+}
