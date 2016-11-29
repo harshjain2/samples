26eb90bbc93bd20cb5407ea91bfbcaa1b30d854d
Harsh Jain <harjain@microsoft.com>
2016-11-29 11:03:54 +0530

    Repor project for NUnit Bug.

---
+++ NUnitBugRepro/UnitTestProject31/UnitTestProject31/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace UnitTestProject3
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [TestCase]
+        [Ignore("Ignore this test case:")]
+        public void TestMethod1()
+        {
+        }
+
+        [TestCase]
+        public void TestMethod2()
+        {
+        }
+
+
+    }
+}
